9f9286a4d172e9504f77c7eae79dfee082c97a14
mbayedestinho-cmd <mbayedestinho@gmail.com>
2026-07-07 09:04:27 +0100

    Update app.py2.py

--- app.py2.py
+++ app.py2.py
@@ -15,7 +15,7 @@ st.set_page_config(
 # --- VOS CONFIGURATIONS ---
 NUMERO_WHATSAPP = "23408167043143"  # Remplacer par votre numéro (sans +)
 MOT_DE_PASSE_ADMIN = "Luxe2026"   # Votre mot de passe secret pour la gestion
-URL_PASSERELLE = "https://script.google.com/macros/s/AKfycbzP9yK0pXvfl6jEb1T4Y65ltd6oFC3dVWwpeyr1x8Nxe2ddtKTncRZ8AHMuxnyllu7S/exec" # Votre lien d'exécution Apps Script
+URL_PASSERELLE = "https://script.google.com/macros/s/AKfycbx3qbgNYzgc6KO98OwppRQ47CeOvXQwJDA16rwCl8r7BtsrOLo-i2C2ilqHoV2-eeg/exec" # Votre lien d'exécution Apps Script
 # 2. Design Haute Couture : Alignement parfait avec votre capture d'écran
 st.markdown("""
     <style>
